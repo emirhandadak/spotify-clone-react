460607474b14457c4b890c9348d1fd11af30c1e5
emirhandadak <emirhan.dadak20039@gmail.com>
2024-05-29 23:41:06 +0300

    initialize project

---
+++ src/assets/assets.js
@@ -0,0 +1,187 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import img7 from './img7.jpg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import img10 from './img10.jpg'
+import img11 from './img11.jpg'
+import img12 from './img12.jpg'
+import img13 from './img13.jpg'
+import img14 from './img14.jpg'
+import img15 from './img15.jpg'
+import img16 from './img16.jpg'
+import song1 from  './song1.mp3'
+import song2 from  './song2.mp3'
+import song3 from  './song3.mp3'
+import song4 from './song4.mp3'
+import song5 from './song5.mp3'
+import song6 from './song6.mp3'
+import song7 from './song7.mp3'
+import song8 from './song8.mp3'
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Top 50 Global",
+        image: img8,
+        desc:"Your weekly update of the most played tracks - Global",
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "Top 50 Turkey",
+        image: img9,
+        desc:"Your weekly update of the most played tracks - Turkey",
+        bgColor:"#22543d"
+    },
+    {   
+        id:2,
+        name: "Viral 50 - Turkey",
+        image: img10,
+        desc:"Your daily update for the most viral tracks right now - Turkey",
+        bgColor:"#742a2a"
+    },
+    {   
+        id:3,
+        name: "Trending Global",
+        image: img16,
+        desc:"Your weekly update of the most played tracks right now - Global",
+        bgColor:"#44337a"
+    },
+    {   
+        id:4,
+        name: "This Is Motive",
+        image: img11,
+        desc:"This is Motive. The most important tracks, all in one playlist.",
+        bgColor:"#234e52"
+    },
+    {   
+        id:5,
+        name: "NKVT",
+        image: img15,
+        desc:"Turkey's tightest playlist Cover : ElMusto",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Say Goodbye 2.0",
+        image: img1,
+        file:song1,
+        desc:"Motive,Ufo361,YKKE",
+        duration:"3:29"
+    },
+    {
+        id:1,
+        name: "A La Carte",
+        image: img2,
+        file:song2,
+        desc:"Segah,Kleo,Motive",
+        duration:"2:35"
+    },
+    {
+        id:2,
+        name: "I Ain’t Worried",
+        image: img3,
+        file:song3,
+        desc:"OneRepublic",
+        duration:"2:29"
+    },
+    {
+        id:3,
+        name: "FEİN!",
+        image: img4,
+        file:song4,
+        desc:"Travis Scott,Playboi Carti",
+        duration:"3:14"
+    },
+    {
+        id:4,
+        name: "Leben Lang",
+        image: img5,
+        file:song5,
+        desc:"Mero",
+        duration:"2:13"
+    },
+    {
+        id:5,
+        name: "MAGAZIN",
+        image: img6    ,
+        file:song6,
+        desc:"UZI,Ufo361",
+        duration:"3:03"
+    },
+    {
+        id:6,
+        name: "EXORCIST",
+        image: img7,
+        file:song7,
+        desc:"Motive",
+        duration:"3:50"
+    },
+    {
+        id:7,
+        name: "SEZEN AKSU",
+        image: img12,
+        file:song8,
+        desc:"LVBEL C5",
+        duration:"2:30"
+    }
+]
